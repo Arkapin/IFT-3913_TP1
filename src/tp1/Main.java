a14d208f28f688724e4621b7d8bd4404d99612c2
Arkapin <gaguifire@hotmail.ca>
2022-02-06 16:45:23 -0500

    initial commit

---
+++ src/tp1/Main.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package tp1;
+
+/**
+ * @author Arkapin£, Guillaume Gagnon, 20191696
+ * @author , Pierre-Paul Hamon, 
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+	}
+
+}
