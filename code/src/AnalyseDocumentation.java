f25350d8376d830344883e20e89448b0459cded3
PPHdUni <36003425+PPHdUni@users.noreply.github.com>
2022-02-08 21:15:34 -0500

    Mise à jour 8 février #1

--- code/src/AnalyseDocumentation.java
+++ code/src/AnalyseDocumentation.java
@@ -17,29 +17,34 @@ public class AnalyseDocumentation {
 
         int loc=0,
                 cloc=0;
-        float dc=0;
+        float dc;
         boolean commentBlock=false,
-                commentLine=false;
+                commentLine;
         File javaFile = new File(path);
         Scanner reader = new Scanner(javaFile);
         String line;
 
         while(reader.hasNextLine()) {
+            commentLine = false;
             line=reader.nextLine();
             System.out.println(line);
             if(line.equals("\n")) continue;
 
             if (line.contains("//")) commentLine = true;
-            if (line.contains("/*") && !commentBlock) commentBlock = true;
+            if (line.contains("/*")) commentBlock = true;
             if (commentBlock) commentLine = true;
-            if (line.contains("*/") && commentBlock) commentBlock = false;
+            if (line.contains("*/")) commentBlock = false;
 
             loc++;
             if(commentLine) cloc++;
         }
 
+        dc = (float) cloc/loc;
+
         reader.close();
         System.out.println(loc);
+        System.out.println(cloc);
+        System.out.println(dc);
 
     }
 
