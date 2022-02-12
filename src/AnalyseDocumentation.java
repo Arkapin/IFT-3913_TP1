6fd6a7af5137878e733046213bc002fe4d2aef4b
Arkapin <gaguifire@hotmail.ca>
2022-02-11 20:54:40 -0500

    Refactoring + documentation
    Refactoring pour respecter les conventions Java et ajout de documentation pour la javadoc

--- src/AnalyseDocumentation.java
+++ src/AnalyseDocumentation.java
@@ -4,25 +4,32 @@ import java.util.*;
 import javax.swing.JFileChooser;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
+/** 
+ * This class provides tools used to compute metrics on Java programs
+ * @author Guillaume Gagnon  20191696
+ * @author Pierre-Paul Hamon 20160518
+ * */
 public class AnalyseDocumentation {
-    
-    //TEST
+
 	private static ArrayList<Metrique> infoClasses = new ArrayList<Metrique>();
 	private static ArrayList<Metrique> infoPaquets = new ArrayList<Metrique>();
-    //TEST
-
+	
+	/**
+	 * Entry point of the program, asks the user for the path, and then start the analysis on the code. Outputs the metrics in csv files.
+	 * @param args The command line arguments.
+	 * @throws java.io.IOException if we can't read/write a file
+	 **/
     public static void main(String[] args) throws IOException {
         System.out.println("Give the path towards a folder that contains Java code");
     	PathInfo pInfo = getPath();
 
-        if(pInfo!=null)
+        if(pInfo != null)
         {
             if(pInfo.isFile())
-                ParseClass(pInfo.getPath());
+                parseClass(pInfo.getPath());
             else if(pInfo.isDirectory()) {
-                // TODO Add logic to handle recursive search of files
                 File f = new File(pInfo.getPath());
-                SearchDirectory(f);
+                searchDirectory(f);
             }
     	}
         else System.out.println("No valid path was given");
@@ -34,20 +41,12 @@ public class AnalyseDocumentation {
     	if(!infoPaquets.isEmpty())
             GenerateurSortie.GenererFichier(infoPaquets, false);
 
-//      Scanner consoleReader = new Scanner(System.in);
-//      System.out.println("Veuillez donner le chemin d'accès d'un dossier qui contient du code Java:");
-//      String path = consoleReader.nextLine();
-//      consoleReader.close();
-        
-//      testTemporaire();
-
-//      TEST
-//      if(path!=null) ParseClass(path);
-//      TEST
-
     }
     
-    /** Returns the path selected by the user, or null if operation was canceled */
+    /** 
+     * Prompts user to select a file or directory to analyze
+     * @return a {@link PathInfo PathInfo} object containing the path selected by the user, or null if operation was canceled 
+     * */
     public static PathInfo getPath() {
     	
     	JFileChooser chooser = new JFileChooser();
@@ -59,27 +58,35 @@ public class AnalyseDocumentation {
         return chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION ? new PathInfo(chooser.getSelectedFile()) : null;
     }
 
-    public static void SearchDirectory(File f) throws IOException {
+    /** 
+     * Recursively searches a file's directory to find other java files in sub-folders
+     * */
+    public static void searchDirectory(File f) throws IOException {
         File[] directory = f.listFiles();
         assert directory != null;
         for (File file: directory)
         {
             if(file.isDirectory()) {
-                SearchDirectory(file);
+                searchDirectory(file);
             }
             else {
-                ParseClass(file.getAbsolutePath());
+            	String fileName = file.getName();
+
+            	int i = fileName.lastIndexOf('.');
+            	if (i > 0) {
+            		if(fileName.substring(i + 1) == "java")
+            			parseClass(file.getAbsolutePath());            			
+            	}
             }
         }
     }
 
-    public static void ParseClass(String path) throws IOException {
+    public static void parseClass(String path) throws IOException {
 
         if(!path.endsWith(".java")) return;
 
-        int loc = 0, cloc = 0, weight=-1;
-        boolean commentBlock=false,
-                commentLine;
+        int loc = 0, cloc = 0, weight = -1;
+        boolean commentBlock = false, commentLine;
         File javaFile = new File(path);
         String name = javaFile.getName();
 
@@ -95,7 +102,7 @@ public class AnalyseDocumentation {
             if(line.trim().equals("")) continue;
 
             if(line.startsWith("package")) {
-                packagePath=line.substring(7).trim();
+                packagePath = line.substring(7).trim();
                 classPackage = packagePath.split("\\.");
             }
 
@@ -112,52 +119,38 @@ public class AnalyseDocumentation {
 
         reader.close();        
 
-        // TEST
     	infoClasses.add(new Metrique(path, name, loc, cloc, weight));
-        // TEST
 
-        if(classPackage!=null) ParsePackage(path,classPackage,loc,cloc,weight);
+        if(classPackage!=null) parsePackage(path, classPackage, loc, cloc, weight);
 
     }
 
-    public static void ParsePackage(String path, String[] classPackage, int loc, int cloc, int weight)
+    public static void parsePackage(String path, String[] classPackage, int loc, int cloc, int weight)
     {
         String packagePath;
         int index;
         Metrique pack;
         for(String packName: classPackage) {
             index = path.lastIndexOf(packName);
-            packagePath = path.substring(0,index+packName.length());
+            packagePath = path.substring(0, index + packName.length());
             pack = null;
 
-            for(int i=0;i < infoPaquets.size(); i++)
+            for(int i = 0; i < infoPaquets.size(); i++)
             {
                 if(infoPaquets.get(i).path.equals(packagePath))
                 {
                     pack = infoPaquets.get(i);
-                    pack.LOC+=loc;
-                    pack.CLOC+=cloc;
-                    pack.weighted+=weight;
+                    pack.LOC += loc;
+                    pack.CLOC += cloc;
+                    pack.weighted += weight;
                     infoPaquets.set(i,pack);
                 }
             }
-            if(pack==null)
+            if(pack == null)
             {
                 pack = new Metrique(packagePath, packName, loc, cloc, weight);
                 infoPaquets.add(pack);
             }
         }
     }
-    
-//    private static void testTemporaire() {
-//    	ArrayList<Metrique> classes = new ArrayList<Metrique>();
-//
-//    	classes.add(new Metrique() {{path = "testPath1"; name = "testNom1"; LOC = 10; CLOC = 2; weighted = 1;}});
-//    	classes.add(new Metrique() {{path = "testPath2"; name = "testNom2"; LOC = 10; CLOC = 4; weighted = -1;}});
-//    	classes.add(new Metrique() {{path = "testPath3"; name = "testNom3"; LOC = 10; CLOC = 6; weighted = 2;}});
-//    	classes.add(new Metrique() {{path = "testPath4"; name = "testNom4"; LOC = 10; CLOC = 8; weighted = -2;}});
-//    	
-//    	GenerateurSortie.GenererFichier(classes, true);
-//    }
-
 }
